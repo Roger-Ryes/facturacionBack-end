7308f9c202f0559a51d16aa738450bc343da6ad2
Jean Rave <jeanclon200021@gmail.com>
2021-03-25 15:37:16 -0500

    Creacion de las carpetas custom y scripts

---
+++ scripts/gd_cliente.sql
@@ -0,0 +1,17 @@
+use cobis
+go
+
+if object_id('gd_cliente') is not null
+	drop table gd_cliente
+go
+
+create table gd_cliente(
+	cl_secuencial      	int 		 not null,
+	cl_cedula       	char	(10) not null,
+	cl_nombre			varchar	(30) not null,
+	cl_apellido 		varchar	(30) not null,
+	cl_telefono			char	(10) not null,
+	cl_direccion		varchar	(10),
+	cl_estado          	char 	(1)  not null
+)
+
