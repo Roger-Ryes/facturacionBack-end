b96620f5836f25d156e6cc56d62ccfe2d5b71d21
Roger-Rys <roger.reyesm97@gmail.com>
2021-03-28 15:25:26 -0500

    etiqueta factura

--- menu-es_EC.js
+++ menu-es_EC.js
@@ -4,7 +4,8 @@
 			"GRUPOD_FASE4":"Taller final GD",
 			"MDL_PRODUCTOS":"M\u00F3dulo Productos",
 			"MDL_CLIENTE":"M\u00F3dulo Cliente",
-			"MDI_CLIENTE":"Insertar Cliente"
+			"MDI_CLIENTE":"Insertar Cliente",
+			"MDL_FACTURA":"Factura"
 			
         }
     }
