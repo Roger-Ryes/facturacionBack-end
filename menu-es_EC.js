6e86225a93cc85483f70d0636c8a7e0621b1d2a6
Roger-Rys <roger.reyesm97@gmail.com>
2021-03-28 12:02:15 -0500

    etiquetas menu cliente

--- menu-es_EC.js
+++ menu-es_EC.js
@@ -2,7 +2,10 @@
     "COMMONS": {
         "MENU": {
 			"GRUPOD_FASE4":"Taller final GD",
-			"MDL_PRODUCTOS":"M\u00F3dulo Productos"
+			"MDL_PRODUCTOS":"M\u00F3dulo Productos",
+			"MDL_CLIENTE":"M\u00F3dulo Cliente"
+			"MDI_CLIENTE":"Insertar Cliente"
+			
         }
     }
 }
